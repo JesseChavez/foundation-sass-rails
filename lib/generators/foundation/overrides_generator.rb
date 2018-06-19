7d3af9610075237e7648e38bd877ad9fe7f5e64b
Jesse Chavez <jesse.chavez.r@gmail.com>
2018-06-20 08:08:00 +1000

    add generator

---
+++ lib/generators/foundation/overrides_generator.rb
@@ -0,0 +1,39 @@
+require 'rails/generators'
+
+module Foundation
+  module Generators
+    class OverridesGenerator < ::Rails::Generators::Base
+      # creates the foundation overrides file
+      def add_overrides
+        create_file overrides_file, File.read(settings_file)
+        append_to_file overrides_file, "\n@import 'foundation';\n"
+      end
+
+      private
+
+      def settings_file
+        File.join(vendor_stylesheets_base_dir, '_settings.scss')
+      end
+
+      def overrides_file
+        File.join(stylesheets_base_dir, 'foundation_and_overrides.scss')
+      end
+
+      def vendor_stylesheets_base_dir
+        File.join(
+          File.dirname(__FILE__),
+          '..',
+          '..',
+          '..',
+          'vendor',
+          'assets',
+          'stylesheets'
+        )
+      end
+
+      def stylesheets_base_dir
+        File.join('app', 'assets', 'stylesheets')
+      end
+    end
+  end
+end
